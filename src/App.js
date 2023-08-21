9fd7089a3785bcd991d7335fd785009abfdf3d58
Oluwole Eesuola <olueesuola@gmail.com>
2023-08-21 23:21:25 +0100

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Images gallery</h1>
+      <Header title='Images Gallery'/>
     </div>
   );
 }
